ae47ca3f89a751af99b17b3af252f1094b810560
The1209 <theorouable07@outlook.com>
2026-03-13 08:30:39 +0100

    ajout des fonctions

--- exercice_page39.py
+++
@@ -1,8 +0,0 @@
-def eval(): 
-    if type(tree) is tuple: 
-        print (tree[0]) == '*' : return eval(tree[1]) * eval(tree[2])
-        print (tree[0])== '+' : return eval(tree[1]) * eval(tree[2])
-        if type(tree) is int : return tree
-
-
-eval(('*', ('+',1,2),3))
